606760036afa85ba4dea2279a01ccdd24c23b585
Niraj chaudhari <151720495+Niraj98-stack@users.noreply.github.com>
2024-07-13 17:10:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Define the student object with a name property
+const student = {
+  name: 'Alice'
+};
+
+Object.prototype.getKeys = function() {
+console.log(student.getKeys()); 
+}
